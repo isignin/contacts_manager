0bc4d3488289f20eacacb1be7b2497f70881a4fc
Steve Loo <isignin@gmail.com>
2015-11-11 21:36:53 -0500

    Using sub modules

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,17 +1,23 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
 	List.Contact = Marionette.ItemView.extend({
-		tagName: "li",
+		tagName: "tr",
 		template: "#contact-list-item",
 		events: {
-		    "click span": "showPhoneNumberAlert"
+		    "click": "highlightName",
+		    "click button": function(){
+			   alert("delete button was clicked");
+		     }
 	      },
-	      showPhoneNumberAlert: function(){
-		     alert(this.model.escape("phoneNumber"));
+	      highlightName: function(e){
+		     this.$el.toggleClass("warning");
 	      }
 	});
 	
-	List.Contacts = Marionette.CollectionView.extend({
-		tagName: "ul",
-		childView: List.Contact
+	List.Contacts = Marionette.CompositeView.extend({
+		tagName: "table",
+		className: "table table-hover",
+		template: '#contact-list',
+		childView: List.Contact,
+		childViewController: "tbody"
 	});
 });
