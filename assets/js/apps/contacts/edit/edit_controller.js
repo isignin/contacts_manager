55d5f532d97ae1b8fa59f7a7d510277ac782399c
Steve Loo <isignin@gmail.com>
2015-11-19 20:47:31 -0500

    Adding edit view along with navigation

---
+++ assets/js/apps/contacts/edit/edit_controller.js
@@ -0,0 +1,24 @@
+ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager, Backbone, Marionette, $, _){
+    Edit.Controller = {
+	   editContact: function(id){
+		 var loadingView = new ContactManager.Common.Views.Loading({
+		     title: "Artificial Loading Delay",
+		     message: "Data loading is delayed..."	
+		});
+		ContactManager.mainRegion.show(loadingView);
+		
+		var fetchingContact = ContactManager.request("contact:entity", id);
+		$.when(fetchingContact).done(function(contact){
+		   var view;
+		   if(contact !== undefined){
+			  view = new Edit.Contact({
+			     model: contact	
+			  });
+		    } else {
+			   view = new ContactManager.ContactsApps.Show.MissingContact();
+		    }
+		ContactManager.mainRegion.show(view);
+		});
+	   }
+    };	
+});
